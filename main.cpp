955907260de9600cbcd80a4fa2911e36e05ebc81
sergeev.rv <rody.sergeev121@gmail.com>
2022-11-20 23:17:35 +0300

    Шаблон

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define st string
+#define pb push_back
+#define vec vector
+#define all(a) a.begin(), a.end()
+#define ff first
+#define ss second
+#define int long long
+
+using namespace std;
+using vi = vec<int>;
+using vvi = vec<vi>;
+using pi = pair<int, int>;
+using vpi = vec<pi>;
+
+using namespace std;
+
+struct Node {
+
+};
+
+class List {
+
+public:
+    
+};
+
+signed main() {
+    system("chcp 65001");
+
+}
